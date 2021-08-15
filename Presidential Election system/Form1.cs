63f1e3b449b6f9ccd2a92f18763d197373d4fbae
mikiyasET <Mikiyaslemlemu@gmail.com>
2021-08-15 12:50:33 +0300

    Progress Map 1

--- Presidential Election system/Form1.cs
+++ Presidential Election system/Form1.cs
@@ -18,82 +18,93 @@ namespace Presidential_Election_system
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-            Point point = pictureBox1.PointToClient(Cursor.Position);
+            /* Point point = pictureBox1.PointToClient(Cursor.Position);
 
-            int x = point.X;
-            int y = point.Y;
-            Console.WriteLine("X: " + point.X +"         Y:" + point.Y +"\n");
-            if(x >= 128 && x <= 244 && (y >= 216 && y <= 255))
-            { 
-                MessageBox.Show("Tigray");
-            }
-            // X: 113 - 208 | Y: 12 - 53, > 184 23 - 85
-            // X: 130 - 250 | Y: 130 - 170
-            
+             int x = point.X;
+             int y = point.Y;
+             Console.WriteLine("X: " + point.X + "         Y:" + point.Y + "\n");*/
         }
         private void pictureBox1_Hover(object sender, EventArgs e)
         {
             Point point = pictureBox1.PointToClient(Cursor.Position);
-
             int x = point.X;
             int y = point.Y;
-            if (x >= 128 && x <= 244 && (y >= 216 && y <= 255))
+
+            // rule:-  Rule(x1,x2,y1,y1)
+            // rule:-       30,88,10,20
+            if (Rule(123, 244, 216, 255) || Rule(216,244,210,291))
             {
-                Console.WriteLine("Tigray");
-            }else
+                // Tigray
+                Console.WriteLine("Tigray X: " + x + "         Y:" + y + "\n");
+            }
+            else if (Rule( 88,242,259,370) || Rule(88,130,277,308))
             {
-                Console.WriteLine("X: " + x + "         Y:" + y + "\n");
+                // Amahara
+                Console.WriteLine("Amhara X: " + x + "         Y:" + y + "\n");
+            }else if (false)
+            {
+                // dire dawa
+                Console.WriteLine("Dire Dawa X: " + x + "         Y:" + y + "\n");
+
+            }
+            else if (Rule(245,329,219,410) || Rule(278,329,260,340))
+            {
+                // Afar
+                Console.WriteLine("Afar X: " + x + "         Y:" + y + "\n");
+            }
+            else if (false)
+            {
+                // Benshal gulgumz
+            }
+            
+            else if (false)
+            {
+                // somali
+            }
+            else if (false)
+            {
+                // sidama
+            }
+            else if (false)
+            {
+                // oromiya
+            }
+            else if (false)
+            {
+                // addis ababa
+            }
+            else if (false)
+            {
+                // SNNP
+            }
+            else if (false)
+            {
+                // Gambela
+            }
+            else if (false)
+            {
+                // hareri
+            }
+            else
+            {
+                Console.WriteLine("Unkown Region    X: " + x + "         Y:" + y + "\n");
             }
         }
 
-        private void button6_Click(object sender, EventArgs e)
+        private bool Rule(int x1, int x2 , int y1 ,int y2)
         {
-
+            Point point = pictureBox1.PointToClient(Cursor.Position);
+            int x = point.X;
+            int y = point.Y;
+            if (x >= x1 && x <= x2 && (y >= y1 && y <= y2))
+            {
+                return true;
+            }else
+            {
+                return false;
+            }
         }
 
-        private void button5_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void panel2_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void panel3_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void label4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label13_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label14_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label13_Click_1(object sender, EventArgs e)
-        {
-
-        }
-
-        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void iconButton1_Click(object sender, EventArgs e)
-        {
-
-        }
+        
     }
 }
