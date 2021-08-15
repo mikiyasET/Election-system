37ea411f6b4157f8f615f575f2c810848200a5d3
Surafel Zeleke <zsura15@gmail.com>
2021-08-15 14:04:37 +0300

    Progress Map 4

--- Presidential Election system/Form1.cs
+++ Presidential Election system/Form1.cs
@@ -32,74 +32,78 @@ namespace Presidential_Election_system
 
             // rule:-  Rule(x1,x2,y1,y1)
             // rule:-       30,88,10,20
-            if (Rule(123, 244, 216, 255) || Rule(216,244,210,291))
+            if (Rule(123, 244, 216, 255) || Rule(216, 244, 210, 291))
             {
                 // Tigray
                 Console.WriteLine("Tigray X: " + x + "         Y:" + y + "\n");
             }
-            else if (Rule(46,133,305,405))
+            else if (Rule(46, 133, 305, 405))
             {
                 // Benshal gulgumz
                 Console.WriteLine("Benishan Gulgumz X: " + x + "         Y:" + y + "\n");
 
             }
-            else if (Rule( 88,242,259,370) || Rule(88,130,277,308))
+            else if (Rule(88, 242, 259, 370) || Rule(88, 130, 277, 308))
             {
                 // Amahara
                 Console.WriteLine("Amhara X: " + x + "         Y:" + y + "\n");
-            }else if (Rule(307,327,381,389))
+            }
+            else if (Rule(307, 327, 381, 389))
             {
                 // dire dawa
                 Console.WriteLine("Dire Dawa X: " + x + "         Y:" + y + "\n");
 
             }
-            else if (Rule(316,325,393,400))
+            else if (Rule(316, 325, 393, 400))
             {
                 // hareri
                 Console.WriteLine("Hareri X: " + x + "         Y:" + y + "\n");
 
             }
-            else if (Rule(76,178,463,565))
+            else if (Rule(76, 178, 463, 565))
             {
                 // SNNP
                 Console.WriteLine("SNNP X: " + x + "         Y:" + y + "\n");
 
             }
-            else if (Rule(48,344,201,596))
+            else if (Rule(48, 344, 201, 596))
             {
                 // oromiya
                 Console.WriteLine("Oromiya X: " + x + "         Y:" + y + "\n");
 
             }
-            else if (Rule(228,518,340,600))
+            else if (Rule(228, 518, 340, 600))
             {
                 // somali
                 Console.WriteLine("Somali X: " + x + "         Y:" + y + "\n");
 
             }
-            else if (Rule(245,329,219,410) || Rule(278,329,260,340))
+            else if (Rule(245, 329, 219, 410) || Rule(278, 329, 260, 340))
             {
                 // Afar
                 Console.WriteLine("Afar X: " + x + "         Y:" + y + "\n");
             }
-            
-            
-           
-            else if (false)
+
+
+
+            else if (Rule(10, 79, 417, 502))
             {
-                // sidama
+                // Gambela
+                Console.WriteLine("Gambela X: " + x + "         Y:" + y + "\n");
+
             }
-            
+
             else if (false)
             {
                 // addis ababa
             }
-           
-            else if (false)
+
+            else if (Rule(183, 217, 470, 515))
             {
-                // Gambela
+                // sidama
+                Console.WriteLine("Sidama X: " + x + "         Y:" + y + "\n");
             }
-            
+
             else
             {
                 Console.WriteLine("Unkown Region    X: " + x + "         Y:" + y + "\n");
